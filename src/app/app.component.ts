403a140563a53162ed2d8c1c172ca24f8ed1803f
alokps6295 <alokps6295@gmail.com>
2021-09-13 10:58:49 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component, VERSION } from '@angular/core';
-
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  FormArray,
+  Validators
+} from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  viewForm: FormGroup;
+  inputArr;
+  genders;
+  ngOnInit() {
+    this.genders = [
+      {
+        name: 'Male',
+        value: 'M'
+      },
+      {
+        name: 'Female',
+        value: 'F'
+      }
+    ];
+    this.inputArr = [
+      {
+        _id: 'aaaaasssss',
+        first_name: 'Alok',
+        second_name: 'Singh',
+        email: 'alokps6295@gmail.com',
+        gender: 'M'
+      }
+    ];
+    this.viewForm = new FormGroup({
+      email: new FormControl(null, [Validators.required]),
+      first_name: new FormControl(null, [Validators.required]),
+      last_name: new FormControl(null),
+      gender: new FormControl(null, [Validators.required])
+    });
+  }
+
+  addStudents() {
+    if (this.viewForm.status == 'VALID') {
+      console.log(this.viewForm.value);
+
+      this.inputArr.push(this.viewForm.value);
+      console.log(this.inputArr);
+    }
+  }
 }
